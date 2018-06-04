d51fead132c9ec508cc5121bb0183dfd4e068596
Dima Andronik <dmitriwebdesign@gmail.com>
2018-06-04 20:59:30 +0300

    initial commit

---
+++ js/script.js
@@ -0,0 +1,22 @@
+$(function() {
+
+	//modal button event handlers
+	$('[modal-toggle]').on('click', function(e) {
+		var target = $(this).attr('modal-toggle');
+		$(target).fadeIn();
+		$('body').addClass('modal-opened');
+	});
+
+	//overlay
+	$('.overlay, .close-btn').on('click', function(e) {
+		$(this).fadeOut();
+		$('body').addClass('modal-opened');
+	})
+	//script for bottle animation
+	setInterval(function() {
+		var active = $('.soda-container img.active');
+		var sibling = (active.next().length === 0) ? active.prev() : active.next();
+		active.removeClass('active');
+		sibling.addClass('active');
+	}, 4000);
+})
