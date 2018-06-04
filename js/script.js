44ec1c48da20b25876db5cb9ce8228a998a84a01
Dima Andronik <dmitriwebdesign@gmail.com>
2018-06-04 22:59:21 +0300

    latest fixes

--- js/script.js
+++ js/script.js
@@ -9,9 +9,15 @@ $(function() {
 
 	//overlay
 	$('.overlay, .close-btn').on('click', function(e) {
-		$(this).fadeOut();
+		$('.overlay').fadeOut();
 		$('body').removeClass('modal-opened');
 	})
+
+	//stop propagation on
+	$('.about-soda').on('click', function(e) {
+		e.stopPropagation();
+	})
+
 	//script for bottle animation
 	setInterval(function() {
 		var active = $('.soda-container img.active');
@@ -19,4 +25,21 @@ $(function() {
 		active.removeClass('active');
 		sibling.addClass('active');
 	}, 4000);
+	
+	//soda info scroll buttons
+	$('.scroll-btn').on('click', function(e) {
+		var container = $(this).closest('.about-soda');
+		var parent = $(this).closest('[class^="soda-"]');
+		if($(this).hasClass('next')) {
+			console.log(parent.next().offset().top);
+			container.animate({
+				scrollTop: parent.next().offset().top + container.scrollTop()
+			})
+		}
+		else {
+			container.animate({
+				scrollTop: parent.prev().offset().top  container.scrollTop()
+			})
+		}
+	})
 })
